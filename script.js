f7297975db7a2a22d9599716be819dd0a306296a
ly-math <113526055+ly-math@users.noreply.github.com>
2023-12-24 11:23:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,47 @@
 
+const btnContainer = document.querySelector('.btn-container');
 const input1 = document.querySelector('.input1');
 const input2 = document.querySelector('.input2');
-const del = document.querySelector('.del');
 const equal = document.querySelector('.equal');
-
-const operator = document.querySelector('.operator');
-
-let inputValue1 = input1.value;
-let inputValue2 = input2.value;
 
 let x = 0;
 let y = 0;
 
 function btnOnclick(text) {
-    operator.textContent = '';
-    if (x === 0 && input1.value.length < 8){
-            input1.value += text;
+    input2.style.maxHeight = '1000px';
+    if (input1.value === 'error' || input1.value === 'Infinity' || input1.value === 'NaN' || input1.value === 'undefined'){
+        input1.value = '';
     }
-    if (x === 1 && input2.value.length < 8){
-            input2.value += text;
+    input1.value += text;
+    input2.value += text;
+    let inputValue2 = input2.value;
+
+    inputValue2 = inputValue2.replace(/\*/g, '×').replace(/\//g, '÷')
+    input2.value = inputValue2;
+    
+    calculateExp(text);
+}
+function calculateExp(expression){
+    try {
+        const calculate = new Function('return ' + expression );
+
+        const result = calculate();
+        return result;
     }
-}
-
-function plusBtn() {
-    x = 1;
-    y = 1;
-    input1.style.maxHeight = '0px';
-    operator.textContent = '+';
-}
-
-function minusBtn() {
-    x = 1;
-    y = 2;
-    input1.style.maxHeight = '0px';
-    operator.textContent = '-';
-}
-
-function multiplyBtn() {
-    x = 1;
-    y = 3;
-    input1.style.maxHeight = '0px';
-    operator.textContent = 'x';
-}
-
-function devideBtn() {
-    x = 1;
-    y = 4;
-    input1.style.maxHeight = '0px';
-    operator.textContent = '/';
+    catch {
+        return 'error';
+    }
 }
 
 function equalBtn(){
-    x = 0;
-    operator.textContent = '';
-    input1.style.maxHeight = '1000px';
-
-    if (y === 1){
-        let result = Number(input1.value) + Number(input2.value);
-    
-        input1.value = '';
-        input2.value = '';
-    
-        input1.value = result;
-    }
-    else if (y === 2){
-        let result = Number(input1.value) - Number(input2.value);
-    
-        input1.value = '';
-        input2.value = '';
-    
-        input1.value = result;
-    }
-    else if (y === 3){
-        let result = Number(input1.value) * Number(input2.value);
-    
-        input1.value = '';
-        input2.value = '';
-    
-        input1.value = result;
-    }
-    else if (y === 4){
-        let result = Number(input1.value) / Number(input2.value);
-    
-        input1.value = '';
-        input2.value = '';
-    
-        input1.value = result;
-    }
+    input2.value = '';
+    input2.style.maxHeight = '0px';
+    input1.value = calculateExp(input1.value);
 }
 
 function clearBtn() {
-    y = 0;
-    x = 0;
-    input1.style.maxHeight = '1000px';
-    operator.textContent = '';
+    input2.style.maxHeight = '1000px';
     input1.value = '';
     input2.value = '';
 }
+
